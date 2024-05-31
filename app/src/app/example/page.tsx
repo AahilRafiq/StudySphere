d0d658503ee4cbb25b158b7aef2131be2bda2d79
AahilRafiq <aahil.rafiq136@gmail.com>
2024-05-31 21:45:51 +0530

    simple fix

--- app/src/app/example/page.tsx
+++ app/src/app/example/page.tsx
@@ -1,12 +1,21 @@
-import {db} from '@/db/db'
-import {user} from '@/db/schema'
+import { db } from '@/db/db'
+import {User} from '@/db/schema'
+import { log } from 'console'
 
 export default async function() {
-    const users = await db.select().from(user)
+    const users = await db.select().from(User)
+    console.log(users)
+
+    await new Promise((resolve) => 
+        setTimeout(() => {
+            console.log('done')
+            resolve('gg')
+        }, 5000)
+    )
 
     return (
         <div>
-            <p>{JSON.stringify(users)}</p>
+            <p>elloH</p>
         </div>
     )
 }
