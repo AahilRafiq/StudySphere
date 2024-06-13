6865b70c3aff7e6db330f4c58ee1a22a3525ceee
AahilRafiq <aahil.rafiq136@gmail.com>
2024-06-13 12:05:55 +0530

    use env vars for db credentials

--- app/drizzle.config.ts
+++ app/drizzle.config.ts
@@ -1,12 +1,11 @@
 import 'dotenv/config'
 import { defineConfig } from "drizzle-kit";
-import { POSTGRES_URI } from "./dev_vars"
 
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   dbCredentials:{
-    url: POSTGRES_URI
+    url: process.env.POSTGRES_URI
   }
 });
