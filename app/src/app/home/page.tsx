697fe3ec35bba076b99afec06e02eeaa57be2a92
AahilRafiq <aahil.rafiq136@gmail.com>
2024-06-14 08:23:24 +0530

    add info on home page

--- app/src/app/home/page.tsx
+++ app/src/app/home/page.tsx
@@ -2,7 +2,11 @@
 export default function() {
 
   return (
-    <div className="m-6">Home page</div>
+    <div className="m-6">
+      <h2 className=" text-xl font-bold"> Home Page </h2>
+      <p className="text-neutral-600"> Select a group from the sidebar , or join a new group</p>
+      <p className="text-neutral-600">Click the Groups Tab in navbar to join a new group</p>
+    </div>
   );
 }
 
