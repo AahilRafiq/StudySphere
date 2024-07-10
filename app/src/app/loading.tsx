4a3cce5093148617377b34285ddcb680c697a0df
AahilRafiq <aahil.rafiq136@gmail.com>
2024-07-10 13:25:35 +0530

    add loading screens

--- app/src/app/loading.tsx
+++ app/src/app/loading.tsx
@@ -1,5 +1,9 @@
-export default function () {
-    return(
-        <div className="m-5">Loading...</div>
-    )
-}
+const FullscreenSpinner = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-blue-500"></div>
+    </div>
+  );
+};
+
+export default FullscreenSpinner;
